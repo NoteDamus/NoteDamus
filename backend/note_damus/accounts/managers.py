c54793cf3e67624bafcbf9ac912ce93a77c74df1
Agshin Kazimli <akazimli9@hotmail.com>
2021-12-01 16:12:17 +0100

    note_damus: add serializers and viewsets
    Create basic APIs to handle note/image/folder/source
    creation, modification and deletion

--- backend/note_damus/accounts/managers.py
+++ backend/note_damus/accounts/managers.py
@@ -26,11 +26,11 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('first_name', '')
+        extra_fields.setdefault('first_name', 'Super')
         extra_fields.setdefault('last_name','')
         extra_fields.setdefault('user_type',2)
         if extra_fields.get('is_staff') is not True:
             raise ValueError(_('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email=email, password=password, **extra_fields)
---
+++ backend/note_damus/accounts/viewsets.py
@@ -0,0 +1,55 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import (
+    FolderSerializer,
+    NoteSerializer,
+    ImageSerializer,
+    SourceSerializer
+)
+from .models import Folder, Note, Image, Source
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+
+
+class FolderViewSet(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = FolderSerializer
+
+    def get_queryset(self):
+        return self.request.user.folders.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        return Note.objects.all()
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = ImageSerializer
+
+    def get_queryset(self):
+        return Image.objects.all()
+
+
+class SourceViewSet(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = SourceSerializer
+
+    def get_queryset(self):
+        return Source.objects.all()
